c0720514739c3fa258607ebfd4495696c102d326
Nick van Santen <vansantennick@gmail.com>
2022-07-18 20:57:36 +0200

    Moved hyperlink logos

--- src/components/sections/HomeSection.vue
+++ src/components/sections/HomeSection.vue
@@ -3,14 +3,14 @@
 <GenericSection id="home-section">
     <h1>Hi, I'm Nick</h1>
     
-    <p>A Computation Science master student with a background in physics and astronomy. Loves a challenge and has an interest in a wide range of fields, ranging from game development to finance, or from physics to web development. </p>
+    <p>A Computation Science master student with a background in physics and astronomy. Loves a challenge and has an interest in a wide range of fields, ranging from game development to finance, and from physics to web development. </p>
     
     <img src="@/assets/me.jpg" alt="Picture of me" width="200">
 
-    <HyperlinkIcon href="mailto:vansantennick@gmail.com" src_icon="assets/email-icon.jpg" src_alt="Email icon"/>
-    <HyperlinkIcon href="https://github.com/Nickvs99" src_icon="assets/github-logo.png" src_alt="Github logo"/>
-    <HyperlinkIcon href="https://www.linkedin.com/in/nick-van-santen-51a2a8173/" src_icon="assets/linkedin-logo.png" src_alt="LinkedIn logo"/>
-    <HyperlinkIcon href="./docs/Resume - Nick van Santen.pdf" src_icon="assets/cv-icon.jpg" src_alt="CV icon" download/>
+    <HyperlinkIcon href="mailto:vansantennick@gmail.com" src_icon="assets/hyperlink-logos/email-icon.jpg" src_alt="Email icon"/>
+    <HyperlinkIcon href="https://github.com/Nickvs99" src_icon="assets/hyperlink-logos/github-logo.png" src_alt="Github logo"/>
+    <HyperlinkIcon href="https://www.linkedin.com/in/nick-van-santen-51a2a8173/" src_icon="assets/hyperlink-logos/linkedin-logo.png" src_alt="LinkedIn logo"/>
+    <HyperlinkIcon href="./docs/Resume - Nick van Santen.pdf" src_icon="assets/hyperlink-logos/cv-icon.jpg" src_alt="CV icon" download/>
 
 </GenericSection>
 
