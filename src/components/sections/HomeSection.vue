dbe5d8774518ed31db09ba661c2769e3f58c3556
Nick van Santen <vansantennick@gmail.com>
2022-07-18 20:52:16 +0200

    All four hyperlinks are now functional

--- src/components/sections/HomeSection.vue
+++ src/components/sections/HomeSection.vue
@@ -3,14 +3,14 @@
 <GenericSection id="home-section">
     <h1>Hi, I'm Nick</h1>
     
-    <p>A Computation Science master student with a background in physics and astronomy. Loves the challenge and has an interest wide range of fields, ranging from game development to finance, or from physics to web development. </p>
+    <p>A Computation Science master student with a background in physics and astronomy. Loves a challenge and has an interest in a wide range of fields, ranging from game development to finance, or from physics to web development. </p>
     
     <img src="@/assets/me.jpg" alt="Picture of me" width="200">
 
-    <HyperlinkIcon href="https://www.github.com" src_icon="assets/email-icon.jpg" src_alt="Email icon"/>
-    <HyperlinkIcon href="https://www.github.com" src_icon="assets/github-logo.png" src_alt="Github logo"/>
-    <HyperlinkIcon href="https://www.github.com" src_icon="assets/linkedin-logo.png" src_alt="LinkedIn logo"/>
-    <HyperlinkIcon href="https://www.github.com" src_icon="assets/cv-icon.jpg" src_alt="CV icon"/>
+    <HyperlinkIcon href="mailto:vansantennick@gmail.com" src_icon="assets/email-icon.jpg" src_alt="Email icon"/>
+    <HyperlinkIcon href="https://github.com/Nickvs99" src_icon="assets/github-logo.png" src_alt="Github logo"/>
+    <HyperlinkIcon href="https://www.linkedin.com/in/nick-van-santen-51a2a8173/" src_icon="assets/linkedin-logo.png" src_alt="LinkedIn logo"/>
+    <HyperlinkIcon href="./docs/Resume - Nick van Santen.pdf" src_icon="assets/cv-icon.jpg" src_alt="CV icon" download/>
 
 </GenericSection>
 
@@ -18,7 +18,7 @@
 
 <script>
 
-import GenericSection from "./GenericSection.vue"
+import GenericSection from "@/components/sections/GenericSection.vue"
 import HyperlinkIcon from "@/components/HyperlinkIcon.vue"
 
 export default {
