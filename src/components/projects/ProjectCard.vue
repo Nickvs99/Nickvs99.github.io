ca54fa0638b7d90995f9932a4c56147d0fcaa06b
Nick van Santen <vansantennick@gmail.com>
2022-07-25 16:58:36 +0200

    Project card setup

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="project-card">
+        <h2 class="project-title"> {{ title }} </h2>
+        <p class="project-description"> {{ description }} </p>
+
+        <div class="project-keyword-container">
+            <div v-for="keyword in keywords" :key="keyword" class="project-keyword">
+                {{ keyword }}
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    
+    props: {
+        title: String,
+        keywords: Array,
+        description: String,
+    },
+}
+
+</script>
+
+<style>
+
+.project-card {
+    background-color: tomato;
+}
+
+</style>
--- src/components/sections/ProjectSection.vue
+++ src/components/sections/ProjectSection.vue
@@ -2,6 +2,11 @@
 
 <GenericSection id="project-section">
     <h1>Projects</h1>
+
+    <div id="project-card-container">
+        <ProjectCard v-for="project in projects" :key="project" :title="project.title" :description="project.description" :keywords="project.keywords"></ProjectCard>
+    </div>
+
 </GenericSection>
 
 </template>
@@ -9,8 +14,27 @@
 <script>
 
 import GenericSection from "./GenericSection.vue"
+import ProjectCard from "@/components/projects/ProjectCard.vue"
+
 export default {
-  components: { GenericSection }
+  components: { GenericSection, ProjectCard },
+  
+  data() {
+    return {
+      projects: [
+        {
+          title: "MusicMatch",
+          description: "A web app that provides the ability to easily compare music tastes between people.",
+          keywords: ["python", "django", "js", "css", "html"],
+        },
+        {
+          title: "Living world",
+          description: "A procedurally generated tile-based world inhabited by cars and boats.",
+          keywords: ["Unity", "C#"],
+        }
+      ]
+    }
+  }
 }
 
 </script>
