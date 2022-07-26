3c590eb7acbfc2dd1b232a8018c8cdf245ef40a4
Nick van Santen <vansantennick@gmail.com>
2022-07-26 23:20:11 +0200

    Living world project doc

--- src/components/projects/contents/ProjectLivingWorld.vue
+++ src/components/projects/contents/ProjectLivingWorld.vue
@@ -1,16 +1,74 @@
 <template>
-    <div class="green">
-        TEST Livng world
-    </div>
+
+<img src="@/assets/projects/Living_world.gif" alt="Image shows the build and live phase of the world." class="project-img" id="living-world-gif"/>
+
+<p> Living World is a procedural generated tile-based world inhabited by boats and cars.</p>
+
+<h3>World generation</h3>
+
+<p>The world is generated through a few simple steps: </p>
+
+<ol>
+    <li>Initialize a grass world with size n by n</li>
+    <li>Generate a non-crossing bezier curve with a width representing the river.</li>
+    <li>Populate the world with x cities.</li>
+    <li>Connect all cities with highways through a minimum spanning tree computed with Prim’s algorithm.</li>
+    <li>Place houses alongside the road.</li>
+    <li>Trees are placed if they have sufficient distance to the nearest neighbourhood.</li>
+</ol>
+
+
+<h3>Agents</h3>
+<p>
+    The world consists of two types of agents: boats and cars.
+    These agents traverse the world through a set of very simple rules, which combined can already create some interesting dynamics. 
+</p>
+
+<h4>Cars</h4>
+<ol>
+    <li>Pick a random starting house and destination.</li>
+    <li>Compute route with a specialized Dijkstra algorithm, which favours highways.</li>
+    <li>Drive over the route as long as no object is blocking you. Blocking can occur due to other cars or bridges which are open.</li>
+</ol>
+
+<h4>Boats</h4>
+<ol>
+    <li>Pick a random direction along the river.</li>
+    <li>Sail along the river as long as no object is blocking you. Blocking can occur due to other boats or bridges which are closed.</li>
+</ol>
+
+See for yourself at <a href="https://github.com/Nickvs99/Living_World" target="_blank">https://github.com/Nickvs99/Living_World</a>!
+
+<ProjectGithubLink href="https://github.com/Nickvs99/Living_World"></ProjectGithubLink>
+
 </template>
 
 <script>
 
+import ProjectGithubLink from "@/components/projects/ProjectGithubLink.vue"
+
+export default {
+    components: {ProjectGithubLink},
+}
+
 </script>
 
-<style>
+<style lang="scss" scoped>
 
-.green {
-    background-color: green;
+@import "@/components/projects/project-styles.scss";
+
+#living-world-gif {
+    float:right;
+
+    @media(max-width: $lg-screen-width) {
+        max-width: 60%;
+    }
+
+    @media(max-width: $md-screen-width) {
+        display: block;
+        float: none;
+        max-width: 100%;
+        margin: 0 auto;
+    }
 }
 </style>
