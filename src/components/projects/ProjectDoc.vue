a6abc737f772e80ca1b0d0ebb632bed99e2c0070
Nick van Santen <vansantennick@gmail.com>
2022-07-26 16:24:33 +0200

    Project doc basic design

--- src/components/projects/ProjectDoc.vue
+++ src/components/projects/ProjectDoc.vue
@@ -3,7 +3,6 @@
 <div ref="background" class="project-doc-background" :class="{hidden: this.disabled}">
     <div class="project-doc" >
         <h2 class="project-doc-title"> {{ title }} </h2>
-        <p class="project-description"> {{ description }} </p>
 
         <div class="project-doc-keyword-container">
             <ProjectKeyword v-for="keyword in keywords" :key="keyword" :keyword="keyword"></ProjectKeyword>
@@ -105,11 +104,27 @@ export default {
 
 .project-doc {
 
-    background-color: white;
+    /* Styling is identical to .project-card for now */
+    background-color: $primary-color;
+    color: $bg-color;
+
+    border: 2px solid $primary-color-darkest;
+    border-radius: 20px;
+
+    padding: 10px 10px;
+
     overflow-y: auto;
 
     width: 100%;
     max-height: 100%;
+}
+
+.project-doc-title {
+    margin: 15px 0;
+}
+
+.project-doc-keyword-container {
+    margin: 15px 0;
 }
 
 </style>
