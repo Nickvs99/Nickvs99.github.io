b2a9846e2a7dcb41cf61a922ba88123a23c9d0f6
Nick van Santen <vansantennick@gmail.com>
2022-08-01 17:29:02 +0200

    Functional education navbar

--- src/components/sections/EducationSection.vue
+++ src/components/sections/EducationSection.vue
@@ -4,15 +4,15 @@
     
     <h1>Education</h1>
     <div id="school-navbar">
-        <div class="school-navbar-item active">Msc Computational science</div>
-        <div class="school-navbar-item">BSc Physics and Astronomy</div>
-        <div class="school-navbar-item">High school</div>
+        <div class="school-navbar-item" id="school-navbar-item-msc" @click="showEducationSection('msc')">MSc Computational science</div>
+        <div class="school-navbar-item" id="school-navbar-item-bsc" @click="showEducationSection('bsc')">BSc Physics and Astronomy</div>
+        <div class="school-navbar-item" id="school-navbar-item-high-school" @click="showEducationSection('high-school')">High school</div>
     </div>
 
     <div id="school-content-container">
-        <EducationMSc class="school-content"></EducationMSc>
-        <EducationBSc class="school-content hidden"></EducationBSc>
-        <EducationHighSchool class="school-content hidden"></EducationHighSchool>
+        <EducationMSc class="school-content" id="school-content-msc"></EducationMSc>
+        <EducationBSc class="school-content" id="school-content-bsc"></EducationBSc>
+        <EducationHighSchool class="school-content" id="school-content-high-school"></EducationHighSchool>
     </div>
 
 </GenericSection>
@@ -27,7 +27,38 @@ import EducationMSc from "@/components/education/EducationMSc.vue"
 import GenericSection from "./GenericSection.vue"
 
 export default {
-  components: { EducationHighSchool, EducationBSc, EducationMSc, GenericSection },
+    components: { EducationHighSchool, EducationBSc, EducationMSc, GenericSection },
+
+    mounted() {
+        this.showEducationSection("msc");
+    },
+
+    methods: {
+
+        hideAllSections() {
+            let navbarItems = document.getElementsByClassName("school-navbar-item");
+            let schoolContents = document.getElementsByClassName("school-content");
+
+            for(let navbarItem of navbarItems) {
+                navbarItem.classList.remove("active");
+            }
+
+            for(let schoolContent of schoolContents) {
+                schoolContent.classList.add("hidden");
+            }
+        },
+
+        showEducationSection(section) {
+
+            this.hideAllSections();
+
+            let navbarItem = document.getElementById("school-navbar-item-" + section);
+            let schoolContent = document.getElementById("school-content-" + section);   
+
+            navbarItem.classList.add("active");
+            schoolContent.classList.remove("hidden");
+        },
+    },
 }
 
 </script>
@@ -39,4 +70,25 @@ export default {
     color: $bg-color;
 }
 
+#school-navbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 10px;
+}
+
+.school-navbar-item {
+
+    // For easy prototyping
+    --padding: 10px;
+    --bottom-border: 5px;
+
+    background-color: $secondary-color;
+    padding: 10px;
+    padding-bottom: calc(var(--padding) - var(--bottom-border));
+
+    &.active {
+        border-bottom: var(--bottom-border) solid $secondary-color-dark;
+    }
+}
 </style>
