71aa87293893cdffb008aeec1c5795c8f96d71de
Nick van Santen <vansantennick@gmail.com>
2022-08-03 16:40:15 +0200

    Send message through contact form

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -2,6 +2,50 @@
 
 <GenericSection id="contact-section">
     <h1>Contact</h1>
+
+    <form class="gform" method="POST" 
+        action="https://script.google.com/macros/s/AKfycbwRSKg41eBdC3XX2BihwRH-EdzZlqQx4Yqqsus_sXwHNDzcziimJ6ByyTg3T7zZa74G6g/exec"
+    >
+
+        <div>
+            <label for="name">Name: </label>
+            <input id="name" name="name" placeholder="What your Mom calls you" />
+        </div>
+
+        <div>
+            <label for="message">Message: </label>
+            <textarea id="message" name="message" rows="10"
+                placeholder="Tell us what's on your mind..."></textarea>
+        </div>
+
+        <div>
+            <label for="email"><em>Your</em> Email Address:</label>
+            <input id="email" name="email" type="email" value=""
+                required placeholder="your.name@email.com"/>
+        </div>
+
+        <div>
+            <label for="color">Favourite Color: </label>
+            <input id="color" name="color" placeholder="green" />
+        </div>
+
+        <div class="pure-group honeypot-field">
+            <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
+            <input id="honeypot" type="text" name="honeypot" value="" />
+        </div>
+
+        <button>
+            &nbsp;Send
+        </button>
+
+
+    </form>
+
+
+
+
+
+
 </GenericSection>
 
 </template>
