d2b4c3895eacb15fe69d7ba8f9bbfe98a65fbda7
Nick van Santen <vansantennick@gmail.com>
2022-08-06 11:42:01 +0200

    Resize hexgrid such that no 3, 1 shape can occur

--- src/components/sections/HomeSection.vue
+++ src/components/sections/HomeSection.vue
@@ -4,11 +4,11 @@
 
     <div id="home-content">
         
-        <div id="home-content-left">
+        <div id="home-content-left" ref="contentContainer">
             <h1 id="title-greeting">Hi, I'm Nick</h1>
             <p id="summary-paragraph">A <b>Computational Science</b> master student with a bachelor level background in <b>Physics and Astronomy</b>. Loves a challenge and has a great interest in a wide range of fields, ranging from game development to finance, and from physics to web development. </p>
             
-            <HexagonGrid hexRadius="55" hexGridAlign="center" hexGridStyle="even-right">
+            <HexagonGrid class="grid" ref="hexGrid" hexRadius="55" hexGridAlign="center" hexGridStyle="even-right">
                 <HyperlinkIcon href="mailto:vansantennick@gmail.com" src_icon="assets/hyperlink-logos/email-icon.jpg" src_alt="Email icon"/>
                 <HyperlinkIcon href="https://github.com/Nickvs99" src_icon="assets/hyperlink-logos/github-logo.png" src_alt="Github logo"/>
                 <HyperlinkIcon href="https://www.linkedin.com/in/nick-van-santen-51a2a8173/" src_icon="assets/hyperlink-logos/linkedin-logo.png" src_alt="LinkedIn logo"/>
@@ -32,6 +32,39 @@ import HyperlinkIcon from "@/components/HyperlinkIcon.vue"
 
 export default {
     components: { GenericSection, HexagonGrid, HyperlinkIcon },
+
+    mounted() {
+        window.addEventListener("resize", this.onResize);
+        this.resizeHexGrid();
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.onResize);
+    },
+
+    methods: {
+
+        resizeHexGrid() {
+            let container = this.$refs.contentContainer;    
+            let availableWidth = container.getBoundingClientRect().width;
+            
+            // Resize the grid for values between 286 and 382 pixels. For these values the hex grid takes
+            // a 3, 1 shape, which looks horrendous. Therefore manually resize, such that the grid makes
+            // a 2, 2 shape.
+            if(availableWidth <= 285 || availableWidth >= 382) {
+                this.$refs.hexGrid.$el.style.width = 100 + "%";
+            }
+            else {
+                this.$refs.hexGrid.$el.style.width = 285 + "px";
+            }
+
+            // Recalculate grid positions based on new width
+            this.$refs.hexGrid.initializeGrid();        
+        },
+
+        onResize() {
+            this.resizeHexGrid();
+        },
+    }
 }
 
 </script>
@@ -78,6 +111,10 @@ export default {
 
 #title-greeting {
     margin: 0 0 30px 0;
+}
+
+.grid {
+    margin: auto;
 }
 
 </style>
